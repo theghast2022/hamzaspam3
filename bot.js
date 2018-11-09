e80ffa7f10b046e7ceb0af6d7dfb7316f63c5a79
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-09 15:29:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "3";
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
@@ -13,5 +14,22 @@ var channel = "508665667776675845";//ايدي الروم
     client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
     },305);
 })
+
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 client.login('NTA4NjcwNDQ4MDQ1Nzg1MDk5.DsCoMw.Oqq1PJfbvqP2aq9NyEQeiwAg6yw');
